8f156f94acc49c034f7650f189a261d296584c21
frank.gairal <gairal@ekino.com>
2018-01-10 17:06:33 -0500

    feat(collisions): cleaning

--- src/app/components/collision/TileCollider.js
+++ src/app/components/collision/TileCollider.js
@@ -6,53 +6,72 @@ export default class TileCollider {
     this.tiles = new TileResolver(entity.game.level.grid);
   }
 
-  checkX() {
+  check(cb) {
+    // return new Promise((resolve) => {
+    //   const matches = this.tiles.searchByRange(
+    //     this.entity.pos.x, this.entity.pos.x + this.entity.size.x,
+    //     this.entity.pos.y, this.entity.pos.y + this.entity.size.y,
+    //   ).filter(match => match.tile.type && match.tile.type === 'hard');
+
+    //   if (matches && matches.length) resolve(matches);
+    // });
     const matches = this.tiles.searchByRange(
       this.entity.pos.x, this.entity.pos.x + this.entity.size.x,
       this.entity.pos.y, this.entity.pos.y + this.entity.size.y,
-    );
+    ).filter(match => match.tile.type && match.tile.type === 'hard');
 
-    if (!matches) return;
+    if (matches && matches.length) cb(matches);
+  }
 
-    matches.forEach((match) => {
-      if (match.tile.type !== 'hard') return;
-
-      if (this.entity.vel.x > 0) {
-        if (this.entity.pos.x + this.entity.size.x > match.x1) {
-          this.entity.pos.x = match.x1 - this.entity.size.x;
-          this.entity.vel.x = 0;
+  checkX() {
+    // this.check()
+    //   .then((matches) => {
+    //     matches.forEach((match) => {
+    //       if (this.entity.vel.x > 0) {
+    //         if (this.entity.pos.x + this.entity.size.x > match.x1) {
+    //           this.entity.pos.x = match.x1 - this.entity.size.x;
+    //           this.entity.vel.x = 0;
+    //         }
+    //       } else if (this.entity.vel.x < 0) {
+    //         if (this.entity.pos.x < match.x2) {
+    //           this.entity.pos.x = match.x2;
+    //           this.entity.vel.x = 0;
+    //         }
+    //       }
+    //     });
+    //   });
+    this.check((matches) => {
+      matches.forEach((match) => {
+        if (this.entity.vel.x > 0) {
+          if (this.entity.pos.x + this.entity.size.x > match.x1) {
+            this.entity.pos.x = match.x1 - this.entity.size.x;
+            this.entity.vel.x = 0;
+          }
+        } else if (this.entity.vel.x < 0) {
+          if (this.entity.pos.x < match.x2) {
+            this.entity.pos.x = match.x2;
+            this.entity.vel.x = 0;
+          }
         }
-      } else if (this.entity.vel.x < 0) {
-        if (this.entity.pos.x < match.x2) {
-          this.entity.pos.x = match.x2;
-          this.entity.vel.x = 0;
-        }
-      }
+      });
     });
   }
 
   checkY() {
-    const matches = this.tiles.searchByRange(
-      this.entity.pos.x, this.entity.pos.x + this.entity.size.x,
-      this.entity.pos.y, this.entity.pos.y + this.entity.size.y,
-    );
-
-    if (!matches) return;
-
-    matches.forEach((match) => {
-      if (match.tile.type !== 'hard') return;
-
-      if (this.entity.vel.y > 0) {
-        if (this.entity.pos.y + this.entity.size.y > match.y1) {
-          this.entity.pos.y = match.y1 - this.entity.size.y;
-          this.entity.vel.y = 0;
+    this.check((matches) => {
+      matches.forEach((match) => {
+        if (this.entity.vel.y > 0) {
+          if (this.entity.pos.y + this.entity.size.y > match.y1) {
+            this.entity.pos.y = match.y1 - this.entity.size.y;
+            this.entity.vel.y = 0;
+          }
+        } else if (this.entity.vel.y < 0) {
+          if (this.entity.pos.y < match.y2) {
+            this.entity.pos.y = match.y2;
+            this.entity.vel.y = 0;
+          }
         }
-      } else if (this.entity.vel.y < 0) {
-        if (this.entity.pos.y < match.y2) {
-          this.entity.pos.y = match.y2;
-          this.entity.vel.y = 0;
-        }
-      }
+      });
     });
   }
 }
