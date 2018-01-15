eead22bd14819bff5c095c8f0d63fb84a292d89e
frank.gairal <gairal@ekino.com>
2018-01-15 16:38:08 -0500

    feat(trait): adding hitable

---
+++ src/app/components/traits/Hitable.js
@@ -0,0 +1,38 @@
+import Trait from './Trait';
+import Sides from '../core/Sides';
+
+export default class Hitable extends Trait {
+  constructor(entity) {
+    super(entity, 'hitable');
+    this.obstructs = true;
+  }
+
+  obstruct(side, {
+    x1, x2, y1, y2,
+  }) {
+    if (!this.obstructs) {
+      return;
+    }
+
+    switch (side) {
+      case Sides.BOTTOM:
+        this.entity.bottom = y1;
+        this.entity.vel.y = 0;
+        break;
+      case Sides.TOP:
+        this.entity.top = y2;
+        this.entity.vel.y = 0;
+        break;
+      case Sides.LEFT:
+        this.entity.left = x2;
+        this.entity.vel.x = 0;
+        break;
+      case Sides.RIGHT:
+        this.entity.right = x1;
+        this.entity.vel.x = 0;
+        break;
+      default:
+        break;
+    }
+  }
+}
